00c47a95a1f4b6b6c9443733e923894e0bbb691e
MhssMC0 <santosmatheussilva375@gmail.com>
2026-09-22 22:57:06 -0300

    Implementa classe Personagem, Emcapsulamento e Construtores

---
+++ Personagem.java
@@ -0,0 +1,119 @@
+public class Personagem {
+    private String nome;
+    private int vida;
+    private int forca;
+    private int nivel;
+
+    //set da vida
+    public void setVida(int vida){
+        if (vida < 0 || vida > 100) {
+            System.out.println("Erro, vida invalida!");
+            return;
+        }
+        this.vida = vida;
+    }
+
+    //get da vida
+    public int getVida(){
+        return vida;
+    }
+
+    //set do nome
+    public void setNome(String nome){
+        if(nome == null || nome.isEmpty()){
+            System.out.println("Erro, nome vazio!");
+            return;
+        }
+        this.nome = nome;
+    }
+
+    //get do nome
+    public String getNome(){
+        return nome;
+    }
+
+    //set da força
+    public void setForca(int forca){
+        if(forca < 1){
+            System.out.println("Erro, forca invalida!");
+            return;
+        }
+        this.forca = forca;
+    }
+
+    //get da forca
+    public int getForca(){
+        return forca;
+    }
+
+    //set do nivel
+    public void setNivel(int nivel){
+        if(nivel < 1){
+            System.out.println("Erro, nivel invalido!");
+            return;
+        }
+        this.nivel = nivel;
+    }
+
+    //get do nivel
+    public int getNivel() {
+        return nivel;
+    }
+
+    //construtor com parametros
+    public Personagem(String nome, int vida, int forca, int nivel) {
+        setNome(nome);
+        setVida(vida);
+        setForca(forca);
+        setNivel(nivel);
+    }
+
+    //cnstrutor padrão
+    public Personagem(){
+        this.nome = "Vladimir";
+        this.vida = 100;
+        this.forca = 20;
+        this.nivel = 10;
+    }
+    
+    //metodo para o personagem receber dano
+    public void receberDano(int dano){
+        this.vida -= dano;
+
+        if(vida < 0){
+            vida = 0;
+        }
+    }
+
+    //metodo padrão para atacar
+    public void atacar(Personagem alvo){
+        System.out.println(nome + " atacou " + alvo.getNome() + " causando " + forca + " de dano!");
+        alvo.receberDano(forca);
+    }
+
+    //metodo para atacar
+    public void atacar(Personagem alvo, int dano){
+        System.out.println(nome + " atacou " + alvo.getNome() + " causando " + dano + " de dano!");
+        alvo.receberDano(dano);
+    }
+
+    //metodo para ver se o personagem está vivo
+    public boolean estaVivo(){
+        if(vida == 0){
+            System.out.println(this.nome + " Morreu!");
+            return false;
+        }
+
+        System.out.println(this.nome + " Esta vivo!");
+        return true;
+    }
+
+    //metodo para mostrar a ficha dos objetos/personagens
+    public void ficha(){
+        System.out.println("\n" + "=====FICHA=====");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Vida: " + getVida());
+        System.out.println("Forca: " + getForca());
+        System.out.println("Nivel: " + getNivel() + "\n");
+    }
+}
